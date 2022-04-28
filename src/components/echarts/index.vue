5ee65f0976e2f19de16d08e95f83f305d313e651
Charles <1092637484@qq.com>
2022-04-29 00:56:57 +0800

    radar echarts can initially be rendered dynamically

--- src/components/echarts/index.vue
+++ src/components/echarts/index.vue
@@ -4,74 +4,141 @@
 </template>
 
 <script>
+import {host} from "../../common/config";
+
 export default {
   name: 'index',
+  data(){
+    return{
+        chart:'',
+        option: {
+          tooltip: {
+            trigger: 'axis',
+            // 坐标轴指示器，坐标轴触发有效
+            axisPointer: {
+              // 默认为直线，可选为：'line' | 'shadow'
+              type: 'shadow'
+            }
+          },
+          radar: {
+            radius: '66%',
+            center: ['50%', '50%'],
+            splitNumber: 8,
+            splitArea: {
+              areaStyle: {
+                color: 'rgba(127,95,132,.3)',
+                opacity: 1,
+                shadowBlur: 45,
+                shadowColor: 'rgba(0,0,0,.5)',
+                shadowOffsetX: 0,
+                shadowOffsetY: 15
+              }
+            },
+            indicator: [
+              {name: 'Scenery', max: 4},
+              {name: 'Repast', max: 4},
+              {name: 'Accommodation', max: 4},
+              {name: 'Shopping', max: 4},
+              {name: 'Entertainment', max: 4},
+              {name: 'Traffic', max: 4}
+            ]
+          },
+          legend: {
+            right: 'center',
+            bottom: '6',
+            data: ['Spot Property']
+          },
+          series: [{
+            type: 'radar',
+            symbolSize: 0,
+            areaStyle: {
+              normal: {
+                shadowBlur: 13,
+                shadowColor: 'rgba(0,0,0,.2)',
+                shadowOffsetX: 0,
+                shadowOffsetY: 10,
+                opacity: 1
+              }
+            },
+            data: [
+              {
+                value: [0, 1, 2, 3, 4, 3],
+                name: 'Spot Property'
+              }
+            ]
+
+          }]
+        },
+
+    }
+  },
+  created() {
+
+  },
   mounted() {
     this.initChart()
+    this.getRadarData()
+  },
+  watch: {
+      //观察option的变化
+      option: {
+        handler(newVal, oldVal) {
+          if (this.chart) {
+            if (newVal) {
+              this.chart.setOption(newVal);
+            } else {
+              this.chart.setOption(oldVal);
+            }
+          } else {
+            this.init();
+          }
+        },
+        deep: true //对象内部属性的监听
+      },
   },
   methods: {
     initChart () {
       // 根据自己的id绑定echarts需要的div
       this.chart = this.$echarts.init(this.$el, 'macarons')
-      this.chart.setOption({
-        tooltip: {
-          trigger: 'axis',
-          // 坐标轴指示器，坐标轴触发有效
-          axisPointer: {
-            // 默认为直线，可选为：'line' | 'shadow'
-            type: 'shadow'
-          }
-        },
-        radar: {
-          radius: '66%',
-          center: ['50%', '50%'],
-          splitNumber: 8,
-          splitArea: {
-            areaStyle: {
-              color: 'rgba(127,95,132,.3)',
-              opacity: 1,
-              shadowBlur: 45,
-              shadowColor: 'rgba(0,0,0,.5)',
-              shadowOffsetX: 0,
-              shadowOffsetY: 15
-            }
-          },
-          indicator: [
-            {name: 'Price', max: 10000},
-            {name: 'Environment', max: 20000},
-            {name: 'Distance', max: 20000},
-            {name: 'Trends', max: 20000},
-            {name: 'Comment', max: 20000}
-          ]
-        },
-        legend: {
-          right: 'center',
-          bottom: '5',
-          data: ['Spot Property']
-        },
-        series: [{
-          type: 'radar',
-          symbolSize: 0,
-          areaStyle: {
-            normal: {
-              shadowBlur: 13,
-              shadowColor: 'rgba(0,0,0,.2)',
-              shadowOffsetX: 0,
-              shadowOffsetY: 10,
-              opacity: 1
-            }
-          },
-          data: [
-            {
-              value: [5500, 11000, 12000, 15000, 12000, 12000],
-              name: 'Spot Property'
-            }
-          ],
-          // animationDuration: animationDuration
-        }]
-      })
+      this.chart.setOption(this.option, true)
+    },
+    getRadarData(){
+      fetch(host + "attraction")
+            .then(res => {
+                return res.json();
+            })
+            .then(attraction => {
+                localStorage.setItem("list", JSON.stringify(attraction.data))
+                // console.log(localStorage.getItem("list"))
+                let radarData = attraction.data[0]
+                // console.log(radarData['name']
+                this.option.series = [{
+                    type: 'radar',
+                    symbolSize: 0,
+                    areaStyle: {
+                      normal: {
+                        shadowBlur: 13,
+                        shadowColor: 'rgba(0,0,0,.2)',
+                        shadowOffsetX: 0,
+                        shadowOffsetY: 10,
+                        opacity: 1
+                      }
+                    },
+                    data: [
+                      {
+                        value: [radarData['scenery_score'], radarData['repast_score'], radarData['accommodation_score'], radarData['shopping_score'], radarData['entertainment_score'], radarData['traffic_score']],
+                        name: 'Spot Property'
+                        // name: radarData['name'],
+                      }
+                    ]
+
+                  }]
+
+            });
+
     }
-  }
+  },
+
 }
 </script>
 

ad041eb20f9da020f19f5572dae11c5fe6d53c39
zhangke <906879240@qq.com>
2022-05-14 18:51:57 +0800

    feat: 'nodata' component
    * Display when there is no selected spot

---
+++ src/components/nodata/index.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="container" v-if="!$store.state.selectedSpot">
+    <div class="item">
+      <img src="../../assets/image/exclamation_mark.svg">
+      <span>No data</span>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
+
+<style scoped lang="scss">
+.container{
+  height: 500px;
+  width: 1000px;
+  margin-bottom: 10px;
+  box-shadow: 2px 4px 12px #99a9bf;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  .item{
+    text-align: center;
+    span{
+      display: block;
+      font-weight: 800;
+    }
+  }
+}
+</style>
